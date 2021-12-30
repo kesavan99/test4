b3eb8adbec92b61f9db316a6b60b452a34f0b51d
kesavan99 <kesavanbala99@gmail.com>
2021-12-30 18:00:54 +0530

    first comment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home  from './component/Home';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div>
+      
+         
+      <Home/>
+    </div>    
+    
+  );
+}
+
+export default App;
